6edbcaa53186cd49ca9eaabc63485f4997de9d61
troy <troy@nutrislice.com>
2014-01-30 10:19:02 -0700

    Made it so the map can receive location updates via Jockeyjs events.

--- locations/static/js/mobile/views/GoogleMapView.js
+++ locations/static/js/mobile/views/GoogleMapView.js
@@ -4,20 +4,29 @@
 define([
     'jquery',
     'backbone',
-    'app'
+    'underscore',
+    'app',
+    'lib/jockey'
 ],
-function($, Backbone, app) {
+function($, Backbone, _, app) {
     return Backbone.View.extend({
         id: 'map-canvas',
         events: {
         },
+        map: null,
         markerModels: [],
         allLatitudes: [],
         allLongitudes: [],
+        currentLocationMarker: null,
         initialize: function(options) {
+            _.bindAll(this, 'updateCurrentPositionOnMap');
             var that = this;
             this.markerModels = options.markerModels;
             that.extractAllLatitudesAndLongitudes();
+
+            Jockey.on("updateUserLocation", function(payload) {
+                that.updateCurrentPositionOnMap(that.map, payload);
+            });
         },
         extractAllLatitudesAndLongitudes: function() {
             this.allLatitudes = this.markerModels.map(function(model) {
@@ -54,15 +63,10 @@ function($, Backbone, app) {
                 this.addMarker(markerModel, map, infoWindow);
             }
         },
-        setCurrentLocationIfPossible: function(map, mapDimentions) {
-            var that = this;
-            // Ask for permission to use current location.
-            // Things to note:
-            //   1 - If they've given permission in the past, it may or may not ask them again.
-            //   2 - If they've given permission in the past it may or may not call EITHER callback function.
-            //   3 - The page does not wait for the user to give/deny permission before continuing to render.
-            if (navigator.geolocation) navigator.geolocation.getCurrentPosition(function(pos) {
-                var myloc = new google.maps.Marker({
+        updateCurrentPositionOnMap: function(map, position, mapDimentions) {
+            var updateMapZoomLevel = false;
+            if (this.currentLocationMarker === null) {
+                this.currentLocationMarker = new google.maps.Marker({
                     clickable: false,
                     icon: new google.maps.MarkerImage('//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
                                                                     new google.maps.Size(22,22),
@@ -72,14 +76,37 @@ function($, Backbone, app) {
                     zIndex: 999,
                     map: map
                 });
+                updateMapZoomLevel = true;
+            }
+            var me = new google.maps.LatLng(position.latitude, position.longitude);
+            this.currentLocationMarker.setPosition(me);
 
-                var me = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
-                myloc.setPosition(me);
+            if (updateMapZoomLevel) {
+                if (typeof mapDimentions === 'undefined') {
+                    mapDimentions = {
+                        height: this.$el.height() > 0 ? this.$el.height()-15 : $(window).height() - 15,
+                        width: this.$el.width() > 0 ? this.$el.width()-15 : $(window).width() - 15
+                    };
+                }
+                this.updateMapZoomLevelToIncludeCurrentUserPosition(map, position, mapDimentions);
+            }
+        },
+        setCurrentLocationIfPossible: function(map, mapDimentions) {
+            var that = this;
 
-                that.updateMapZoomLevelToIncludeCurrentUserPosition(map, pos, mapDimentions);
-            }, function(error) {
-                // Map is already rendered w/o the position. Nothing to do...
-            });
+            // If running in the wrapper app, it will ask for location permission separate
+            // from the site (i.e. if the site asks, BOTH will ask), so we'll just let the
+            // app control it instead and pass info to the site.
+            if (!app.isRunningInWrapperApp) {
+                if (navigator.geolocation) navigator.geolocation.getCurrentPosition(function(pos) {
+                    that.updateCurrentPositionOnMap(map, pos.coords, mapDimentions);
+                }, function(error) {
+                    // Map is already rendered w/o the position. Nothing to do...
+                    console.log("Error while retrieving user's current location: " + error);
+                }, {
+                    timeout: 10000
+                });
+            }
         },
         getAverageValues: function(arr) {
             if (!arr || arr.length === 0)
@@ -94,7 +121,7 @@ function($, Backbone, app) {
                 averageLongitude = this.getAverageValues(this.allLongitudes);
             return new google.maps.LatLng(averageLatitude,averageLongitude);
         },
-        getBoundsZoomLevel: function(bounds, mapDim) {
+        getBoundsZoomLevel: function(minLatitude, maxLatitude, minLongitude, maxLongitude, mapDim) {
             // Uses technique from http://stackoverflow.com/questions/6048975/google-maps-v3-how-to-calculate-the-zoom-level-for-a-given-bounds#answer-13274361
             var WORLD_DIM = { height: 256, width: 256 };
             var ZOOM_MAX = 21;
@@ -108,18 +135,22 @@ function($, Backbone, app) {
             function zoom(mapPx, worldPx, fraction) {
                 return Math.floor(Math.log(mapPx / worldPx / fraction) / Math.LN2);
             }
+            var maxLatRad = latRad(maxLatitude),
+                minLatRad = latRad(minLatitude),
+                latFraction = (maxLatRad - minLatRad) / Math.PI;
 
-            var ne = bounds.getNorthEast();
-            var sw = bounds.getSouthWest();
-
-            var latFraction = (latRad(ne.lat()) - latRad(sw.lat())) / Math.PI;
-
-            var lngDiff = ne.lng() - sw.lng();
+            var lngDiff = maxLongitude - minLongitude;
             var lngFraction = ((lngDiff < 0) ? (lngDiff + 360) : lngDiff) / 360;
 
             var latZoom = zoom(mapDim.height, WORLD_DIM.height, latFraction);
             var lngZoom = zoom(mapDim.width, WORLD_DIM.width, lngFraction);
 
+//            setTimeout(function() {
+//                alert('Zoom calculator...\nlatZoom: ' + latZoom + '\nlngZoom: ' + lngZoom
+//                 + '\nZOOM_MAX: ' + ZOOM_MAX + '\nlatFraction: ' + latFraction + '\nlngDiff: '
+//                 + lngDiff + '\nlngFraction: ' + lngFraction + '\nmaxLatRad: ' + maxLatRad
+//                 + '\nminLatRad: ' + minLatRad + '\nmapHeight: ' + mapDim.height + '\nmapWidth: ' + mapDim.width);
+//            }, 6000);
             return Math.min(latZoom, lngZoom, ZOOM_MAX);
         },
         calculateAppropriateZoomLevel: function(mapDimentions) {
@@ -127,16 +158,14 @@ function($, Backbone, app) {
                 minLatitude = _.min(this.allLatitudes),
                 maxLongitude = _.max(this.allLongitudes),
                 minLongitude = _.min(this.allLongitudes),
-                sw = new google.maps.LatLng(minLatitude, minLongitude),
-                ne = new google.maps.LatLng(maxLatitude, maxLongitude),
-                bounds = new google.maps.LatLngBounds(sw, ne),
-                zoom = this.getBoundsZoomLevel(bounds, mapDimentions);
+                zoom = this.getBoundsZoomLevel(minLatitude, maxLatitude, minLongitude, maxLongitude, mapDimentions);
             return zoom;
         },
         updateMapZoomLevelToIncludeCurrentUserPosition: function(map, currentUserPosition, mapDimentions) {
-            this.allLatitudes.push(currentUserPosition.coords.latitude);
-            this.allLongitudes.push(currentUserPosition.coords.longitude);
-            map.setZoom(this.calculateAppropriateZoomLevel(mapDimentions));
+            this.allLatitudes.push(currentUserPosition.latitude);
+            this.allLongitudes.push(currentUserPosition.longitude);
+            var zoom = this.calculateAppropriateZoomLevel(mapDimentions);
+            map.setZoom(zoom);
             map.setCenter(this.calculateCenterOfMarkers());
         },
         render: function() {
@@ -150,9 +179,10 @@ function($, Backbone, app) {
                     content: $infoWindowContent.get(0)
                 }),
                 mapDimentions = {
-                    height: $(window).height()-15,
-                    width: $(window).width()-15
+                    height: this.$el.height() > 0 ? this.$el.height()-15 : $(window).height() - 15,
+                    width: this.$el.width() > 0 ? this.$el.width()-15 : $(window).width() - 15
                 };
+            this.map = map;
             this.addMarkers(this.markerModels, map, infoWindow);
             map.setZoom(this.calculateAppropriateZoomLevel(mapDimentions));
             this.setCurrentLocationIfPossible(map, mapDimentions);
