f21044e7ab38dea14438b8ffab5260c129877eb6
troy <troy@nutrislice.com>
2014-02-03 17:31:48 -0700

    Added model and admin for Hours of Operation.

--- locations/admin.py
+++ locations/admin.py
@@ -1,18 +1,29 @@
 from django.contrib import admin
 from django_google_maps import widgets as map_widgets
 from django_google_maps import fields as map_fields
-from locations.models import MealLocation
+from locations.models import MealLocation, OperatingHours
+
+
+class OperatingHoursInline(admin.TabularInline):
+    model = OperatingHours
+    extra = 0
+    verbose_name = 'Hours of Operation'
+    verbose_name_plural = 'Hours of Operation'
+
 
 class MealLocationAdmin(admin.ModelAdmin):
     formfield_overrides = {
         map_fields.AddressField: {'widget': map_widgets.GoogleMapsAddressWidget},
     }
     list_display = ('name', 'address')
+    inlines = (OperatingHoursInline, )
 
     class Media:
          js = (
              '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
              'customadmin/js/prevent_enter_backspace.js',
              )
+
+
 
 admin.site.register(MealLocation, MealLocationAdmin)
--- locations/models.py
+++ locations/models.py
@@ -1,8 +1,35 @@
+from datetime import datetime
 from django.db import models
 from django_google_maps import fields as map_fields
+from django.utils.translation import ugettext_lazy as _
 
 
 class MealLocation(models.Model):
     name = models.CharField(max_length=254)
     address = map_fields.AddressField(max_length=254)
     geolocation = map_fields.GeoLocationField(max_length=100)
+
+
+WEEKDAYS = [
+    (1, _("Monday")),
+    (2, _("Tuesday")),
+    (3, _("Wednesday")),
+    (4, _("Thursday")),
+    (5, _("Friday")),
+    (6, _("Saturday")),
+    (7, _("Sunday")),
+]
+TIME_FORMAT = "%-I:%M%p"
+
+
+class OperatingHours(models.Model):
+    meal_location = models.ForeignKey(MealLocation, related_name='operating_hours')
+    weekday_from = models.IntegerField(choices=WEEKDAYS, unique=True)
+    weekday_to = models.IntegerField(choices=WEEKDAYS)
+    from_hour = models.TimeField(verbose_name='from')
+    to_hour = models.TimeField(verbose_name='to')
+
+    def __unicode__(self):
+        formatted_from = self.from_hour.strftime(TIME_FORMAT)
+        formatted_to = self.to_hour.strftime(TIME_FORMAT)
+        return '%s-%s %s-%s' % (self.get_weekday_from_display(), self.get_weekday_to_display(), formatted_from, formatted_to)
