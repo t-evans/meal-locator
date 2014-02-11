d0f1a9c303dd0d2eec1b65a5e0b466bacecadc89
troy <troy@nutrislice.com>
2014-02-11 14:40:26 -0700

    Fixed issue where STATIC_URL was an invalid value after adding grappelli.

--- meal_locator/settings.py
+++ meal_locator/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE_CLASSES = (
 TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.request",
+    "django.core.context_processors.static",
 )
 
 
