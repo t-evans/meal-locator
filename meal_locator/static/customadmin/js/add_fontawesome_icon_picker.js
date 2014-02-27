023119a0025f2d82a77f3764f2319a281a85bdc5
troy <troy@nutrislice.com>
2014-02-26 17:31:57 -0700

    Added a bunch more fontawesome icons

--- meal_locator/static/customadmin/js/add_fontawesome_icon_picker.js
+++ meal_locator/static/customadmin/js/add_fontawesome_icon_picker.js
@@ -6,18 +6,151 @@
  */
 
 (function($) {
+    // More icons can be found at http://fontawesome.io/icons/
     var awesomeIconsArray = [
         {
             "label": "Clock",
             "cssClass": "fa fa-clock-o fa-lg"
         },
         {
+            "label": "Cutlery",
+            "cssClass": "fa fa-cutlery fa-lg"
+        },
+        {
             "label": "Facebook",
+            "cssClass": "fa fa-facebook fa-lg"
+        },
+        {
+            "label": "Facebook Square",
             "cssClass": "fa fa-facebook-square fa-lg"
+        },
+        {
+            "label": "Twitter",
+            "cssClass": "fa fa-twitter fa-lg"
+        },
+        {
+            "label": "Twitter Square",
+            "cssClass": "fa fa-twitter-square fa-lg"
+        },
+        {
+            "label": "Instagram",
+            "cssClass": "fa fa-instagram fa-lg"
+        },
+        {
+            "label": "YouTube",
+            "cssClass": "fa fa-youtube fa-lg"
         },
         {
             "label": "Camera",
             "cssClass": "fa fa-camera-retro fa-lg"
+        },
+        {
+            "label": "Calendar",
+            "cssClass": "fa fa-calendar fa-lg"
+        },
+        {
+            "label": "Checkmark",
+            "cssClass": "fa fa-check fa-lg"
+        },
+        {
+            "label": "Checkmark Square",
+            "cssClass": "fa fa-check-square-o fa-lg"
+        },
+        {
+            "label": "Envelope",
+            "cssClass": "fa fa-envelope fa-lg"
+        },
+        {
+            "label": "Info",
+            "cssClass": "fa fa-info fa-lg"
+        },
+        {
+            "label": "Info Circle",
+            "cssClass": "fa fa-info-circle fa-lg"
+        },
+        {
+            "label": "Money",
+            "cssClass": "fa fa-money fa-lg"
+        },
+        {
+            "label": "Exclamation",
+            "cssClass": "fa fa-exclamation fa-lg"
+        },
+        {
+            "label": "Exclamation Triangle",
+            "cssClass": "fa fa-exclamation-triangle fa-lg"
+        },
+        {
+            "label": "Question",
+            "cssClass": "fa fa-question fa-lg"
+        },
+        {
+            "label": "Question Circle",
+            "cssClass": "fa fa-question-circle fa-lg"
+        },
+        {
+            "label": "Phone",
+            "cssClass": "fa fa-phone fa-lg"
+        },
+        {
+            "label": "Phone Square",
+            "cssClass": "fa fa-phone-square fa-lg"
+        },
+        {
+            "label": "Download",
+            "cssClass": "fa fa-download fa-lg"
+        },
+        {
+            "label": "Flag",
+            "cssClass": "fa fa-flag-o fa-lg"
+        },
+        {
+            "label": "Heart",
+            "cssClass": "fa fa-heart fa-lg"
+        },
+        {
+            "label": "External Link",
+            "cssClass": "fa fa-external-link fa-lg"
+        },
+        {
+            "label": "Comments",
+            "cssClass": "fa fa-comments fa-lg"
+        },
+        {
+            "label": "Signal",
+            "cssClass": "fa fa-signal fa-lg"
+        },
+        {
+            "label": "Mobile Device",
+            "cssClass": "fa fa-mobile fa-lg"
+        },
+        {
+            "label": "Star",
+            "cssClass": "fa fa-star fa-lg"
+        },
+        {
+            "label": "Star Outline",
+            "cssClass": "fa fa-star-o fa-lg"
+        },
+        {
+            "label": "Moon",
+            "cssClass": "fa fa-moon-o fa-lg"
+        },
+        {
+            "label": "Lock",
+            "cssClass": "fa fa-lock fa-lg"
+        },
+        {
+            "label": "Bookmark",
+            "cssClass": "fa fa-bookmark fa-lg"
+        },
+        {
+            "label": "Bell",
+            "cssClass": "fa fa-bell fa-lg"
+        },
+        {
+            "label": "Wheelchair",
+            "cssClass": "fa fa-wheelchair fa-lg"
         }
     ];
 
