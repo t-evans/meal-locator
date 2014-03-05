b20298ed00dfb38401da94542bd0517e5a531082
troy <troy@nutrislice.com>
2014-03-05 16:59:59 -0700

    Enabled GeoDjango for the project.

--- meal_locator/settings.py
+++ meal_locator/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'rest_framework',
     'south',
     'django_google_maps',
@@ -75,7 +76,7 @@ WSGI_APPLICATION = 'meal_locator.wsgi.application'
 if LOCAL:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': 'meal-locator',                      # Or path to database file if using sqlite3.
             'USER': 'postgres',                      # Not used with sqlite3.
             'PASSWORD': 'postgres',                  # Not used with sqlite3.
